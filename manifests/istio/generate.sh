ab45b63cf2778c004322bc1df780f05c539cf541
Kevin Ho <kevinkh08@gmail.com>
2026-04-19 13:18:02 -0700

    fixed bug in generate.sh

--- manifests/istio/generate.sh
+++ manifests/istio/generate.sh
@@ -1,20 +1,19 @@
-PWD=$PWD
-BASE="$PWD/base"
-CNI="$PWD/cni"
-ISTIOD="$PWD/istiod"
+ROOT=$PWD
+BASE="$ROOT/base"
+CNI="$ROOT/cni"
+ISTIOD="$ROOT/istiod"
 # TODO: Gateways
 
 VERSION="1.17.2"
 
-
 cd $BASE
 helm template istio-base istio/base -n istio-system --version $VERSION -f values.yaml > generated.yaml
-$PWD/manifest-seperator generated.yaml
+$ROOT/manifest-seperator generated.yaml
 
 cd $CNI
 helm template istio-cni istio/cni -n kube-system --version $VERSION > generated.yaml
-$PWD/manifest-seperator generated.yaml
+$ROOT/manifest-seperator generated.yaml
 
 cd $ISTIOD
 helm template istiod istio/istiod -n istio-system --version $VERSION -f values.yaml > generated.yaml
-$PWD/manifest-seperator generated.yaml
+$ROOT/manifest-seperator generated.yaml
