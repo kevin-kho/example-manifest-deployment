559846e3f2606b39f1d95eab4bfe3485612ba48f
Kevin Ho <kevinkh08@gmail.com>
2026-04-19 12:57:00 -0700

    enable meshwide logging

--- manifests/istio/istiod/cmd.sh
+++ manifests/istio/istiod/cmd.sh
@@ -1,3 +1,2 @@
-# helm template istiod istio/istiod -n istio-system --version 1.17.2 > generated.yaml
 helm template istiod istio/istiod -n istio-system --version 1.17.2 -f values.yaml > generated.yaml
 ./manifest-seperator generated.yaml
