5f4a1c68845aad25e715dadd6c504bc5f2c94105
Kevin Ho <kevinkh08@gmail.com>
2026-04-19 13:11:59 -0700

    simplify the use of manifest-seperator

--- manifests/istio/generate.sh
+++ manifests/istio/generate.sh
@@ -9,12 +9,12 @@ VERSION="1.17.2"
 
 cd $BASE
 helm template istio-base istio/base -n istio-system --version $VERSION -f values.yaml > generated.yaml
-./manifest-seperator generated.yaml
+$PWD/manifest-seperator generated.yaml
 
 cd $CNI
 helm template istio-cni istio/cni -n kube-system --version $VERSION > generated.yaml
-./manifest-seperator generated.yaml
+$PWD/manifest-seperator generated.yaml
 
 cd $ISTIOD
 helm template istiod istio/istiod -n istio-system --version $VERSION -f values.yaml > generated.yaml
-./manifest-seperator generated.yaml
+$PWD/manifest-seperator generated.yaml
