b65b009cdda9f250cb8d6d5f564b9729eabb0394
Kevin Ho <kevinkh08@gmail.com>
2026-04-19 12:52:52 -0700

    added istiod value file and set REGISTRY_ONLY

--- manifests/istio/istiod/cmd.sh
+++ manifests/istio/istiod/cmd.sh
@@ -1,2 +1,3 @@
-helm template istiod istio/istiod -n istio-system --version 1.17.2 > generated.yaml
+# helm template istiod istio/istiod -n istio-system --version 1.17.2 > generated.yaml
+helm template istiod istio/istiod -n istio-system --version 1.17.2 -f values.yaml > generated.yaml
 ./manifest-seperator generated.yaml
